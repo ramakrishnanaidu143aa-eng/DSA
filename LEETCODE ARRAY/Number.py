5b3cd43465fd58a1876a2aa80f75cc20463d06d0
ramakrishnanaidu143aa <ramakrishnanaidu143aa@gmail.com>
2026-04-05 15:08:28 +0530

    missing number

---
+++ LEETCODE ARRAY/Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        xor_all = 0
+        
+        for i in range(n + 1):
+            xor_all ^= i
+        
+        for num in nums:
+            xor_all ^= num
+        
+        return xor_all
